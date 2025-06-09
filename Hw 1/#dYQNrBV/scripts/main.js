bd0cf90ad92799a01f81335f9477f7704c92d2d4
RomanGit777 <rsavenko91@gmail.com>
2025-06-09 20:23:20 +0200

    doing hw

---
+++ Hw 1/#dYQNrBV/scripts/main.js
@@ -0,0 +1,34 @@
+let a = "hello"
+let b = "owu"
+let c = "com"
+let d = "ua"
+let g = "1"
+
+
+let n1 = 1
+let n2 = 10
+let n3 = -999
+let n4 = 123
+let n5 = 3.14
+let n6 = 2.7
+let n7 = 16
+
+let bool1 = true
+let bool2 = false
+
+console.log(a)
+console.log(b)
+console.log(d)
+console.log(c)
+console.log(g)
+
+console.log(n1)
+console.log(n2)
+console.log(n3)
+console.log(n4)
+console.log(n5)
+console.log(n6)
+console.log(n7)
+
+console.log(bool1)
+console.log(bool2)
