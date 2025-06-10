61577087d19c306bd8bbcb3a6c24003b32db58e1
RomanGit777 <rsavenko91@gmail.com>
2025-06-10 16:54:30 +0200

    done with main.js

---
+++ Hw 1/#ruUtWDUI/scripts/main.js
@@ -0,0 +1,7 @@
+let firstName = prompt("Введіть своє ім'я:")
+let middleName = prompt("Введіть своє по батькові:")
+let age = prompt("Введіть свій вік:")
+
+console.log("Введіть своє ім'я:", firstName)
+console.log("Введіть своє по батькові:", middleName)
+console.log("Введіть свій вік:", age)
