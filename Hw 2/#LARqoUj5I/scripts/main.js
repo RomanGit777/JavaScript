a306b8c574289569b2c18ec40f97c1726b1301c3
RomanGit777 <rsavenko91@gmail.com>
2025-06-11 13:58:10 +0200

    done with main.js

---
+++ Hw 2/#LARqoUj5I/scripts/main.js
@@ -0,0 +1,10 @@
+
+let book1 = {title: 'A', pageCount: 123, genre: 'AA'}
+let book2 = {title: 'B', pageCount: 222, genre: 'BB'}
+let book3 = {title: 'C', pageCount: 333, genre: 'CC'}
+
+
+console.log(book1)
+console.log(book2)
+console.log(book3)
+
