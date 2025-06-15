4ad29c61a2b157e8aed256cdefde3f8286af6c5e
RomanGit777 <rsavenko91@gmail.com>
2025-06-15 17:10:35 +0200

    start with main.js

--- Hw 3/#4WrHwFTEop0/scripts/main.js
+++ Hw 3/#4WrHwFTEop0/scripts/main.js
@@ -1,28 +1,28 @@
-let users = [
-
-    {name: `vasya`, age: 31, status: false},
-
-    {name: `petya`, age: 30, status: true},
-
-    {name: `kolya`, age: 29, status: true},
-
-    {name: `olya`, age: 28, status: false},
-
-    {name: `max`, age: 30, status: true},
-
-    {name: `anya`, age: 31, status: false},
-
-    {name: `oleg`, age: 28, status: false},
-
-    {name: `andrey`, age: 29, status: true},
-
-    {name: `masha`, age: 30, status: true},
-
-    {name: `olya`, age: 31, status: false},
-
-    {name: `max`, age: 31, status: true}
-
-];
+// let users = [
+//
+//     {name: `vasya`, age: 31, status: false},
+//
+//     {name: `petya`, age: 30, status: true},
+//
+//     {name: `kolya`, age: 29, status: true},
+//
+//     {name: `olya`, age: 28, status: false},
+//
+//     {name: `max`, age: 30, status: true},
+//
+//     {name: `anya`, age: 31, status: false},
+//
+//     {name: `oleg`, age: 28, status: false},
+//
+//     {name: `andrey`, age: 29, status: true},
+//
+//     {name: `masha`, age: 30, status: true},
+//
+//     {name: `olya`, age: 31, status: false},
+//
+//     {name: `max`, age: 31, status: true}
+//
+// ];
 
 
 // – користувачів зі статусом true
@@ -40,8 +40,36 @@ let users = [
 
 
 // – користувачів, які старші за 30 років
-for (const user of users) {
-    if (user.age > 30) {
-        console.log(user);
-    }
-}
+// for (const user of users) {
+//     if (user.age > 30) {
+//         console.log(user);
+//     }
+// }
+
+// =============
+
+// <div class=”users-box”>
+//
+// <div class=”user-block”>
+//
+// <h2>ID – NAME – USERNAME </h2>
+//
+// <h3>EMAIL – PHONE</h3>
+//
+// <div class=”address-block”>
+//
+// <p>City – ADDRESS.CITY</p>
+//
+// <p>Street – ADDRESS.STREET</p>
+//
+// <p>Suite – ADDRESS.SUITE</p>
+//
+// <p>Zip code – ADDRESS.ZIPCODE</p>
+//
+// </div>
+//
+// </div>
+// <!–
+//             other users
+//             –>
+// за допомогою стилів, побудувати сітку по 2 об’єктах в рядок
