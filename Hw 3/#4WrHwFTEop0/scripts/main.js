3cfa1756a2aa2507746531c40abbc49dbd911024
RomanGit777 <rsavenko91@gmail.com>
2025-06-16 11:32:39 +0200

    working with main.js

--- Hw 3/#4WrHwFTEop0/scripts/main.js
+++ Hw 3/#4WrHwFTEop0/scripts/main.js
@@ -281,53 +281,56 @@ const users = [
     }
 ];
 
-for (const user of users) {
-    const userHTML = `
-  <div class="user-block">
-    <h2>${user.id} – ${user.name} – ${user.username}</h2>
-    <h3>${user.email} – ${user.phone}</h3>
-    <div class="address-block">
-      <p>City – ${user.address.city}</p>
-      <p>Street – ${user.address.street}</p>
-      <p>Suite – ${user.address.suite}</p>
-      <p>Zip code – ${user.address.zipcode}</p>
-    </div>
-  </div>
-`;
+document.addEventListener("DOMContentLoaded", () => {
     const usersBox = document.getElementById("users-box");
-    usersBox.innerHTML += userHTML;
 
-}
+    for (const user of users) {
+        const userHTML = `
+      <div class="user-block">
+        <h2>${user.id} – ${user.name} – ${user.username}</h2>
+        <h3>${user.email} – ${user.phone}</h3>
+        <div class="address-block">
+          <p>City – ${user.address.city}</p>
+          <p>Street – ${user.address.street}</p>
+          <p>Suite – ${user.address.suite}</p>
+          <p>Zip code – ${user.address.zipcode}</p>
+        </div>
+      </div>
+    `;
+        usersBox.innerHTML += userHTML;
+    }
+});
 
 
 
 
 
 
-<div class='users-box'>
 
-<div class='user-block'>
-
-<h2>ID – NAME – USERNAME </h2>
-
-<h3>EMAIL – PHONE</h3>
-
-<div class='address-block'>
-
-<p>City – ADDRESS.CITY</p>
-
-<p>Street – ADDRESS.STREET</p>
-
-<p>Suite – ADDRESS.SUITE</p>
-
-<p>Zip code – ADDRESS.ZIPCODE</p>
-
-</div>
-
-</div>
-
-{/*за допомогою стилів, побудувати сітку по 2 об’єктах в рядок**/}
-
+// <div class='users-box'>
+//
+// <div class='user-block'>
+//
+// <h2>ID – NAME – USERNAME </h2>
+//
+// <h3>EMAIL – PHONE</h3>
+//
+// <div class='address-block'>
+//
+// <p>City – ADDRESS.CITY</p>
+//
+// <p>Street – ADDRESS.STREET</p>
+//
+// <p>Suite – ADDRESS.SUITE</p>
+//
+// <p>Zip code – ADDRESS.ZIPCODE</p>
+//
+// </div>
+//
+// </div>
+//
+// {/*за допомогою стилів, побудувати сітку по 2 об’єктах в рядок**/}
+//
 
 
 
