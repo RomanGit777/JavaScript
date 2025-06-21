b66b37ebc32fa3944cfe43f09bb30015e9ac75d7
RomanGit777 <rsavenko91@gmail.com>
2025-06-21 13:21:15 +0200

    done with main.js

---
+++ hw 3 additionally/#WpkK0ZH1/scripts/main.js
@@ -0,0 +1,8 @@
+const numbers = [22, 33, 44, 54, 64]
+console.log("Числовий масив", numbers);
+
+const string = ["яблуко","груша", "слива", "апельсин", "мандарин"]
+console.log("Стрічковий масив", string);
+
+const mix = [ 1, "two", true, false, 5]
+console.log("Міксований масив", mix)
