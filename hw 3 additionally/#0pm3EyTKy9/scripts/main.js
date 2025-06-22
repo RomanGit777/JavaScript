d55071d9a6fa97d02bb7ae023d4ffee6542b6980
RomanGit777 <rsavenko91@gmail.com>
2025-06-22 14:42:25 +0200

    done with main.js

---
+++ hw 3 additionally/#0pm3EyTKy9/scripts/main.js
@@ -0,0 +1,15 @@
+let arr = [];
+arr[0] = 20
+arr[1] = null
+arr[2] = undefined
+arr[3] = NaN
+arr[4] = true
+arr[5] = 'string'
+arr[6] = 'number'
+arr[7] = 44
+arr[8] = 20
+arr[9] = 12
+
+for (let i = 0; i < arr.length; i++){
+    console.log(arr[i])
+}
