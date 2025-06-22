ec30c6a3ba3910f6d11548deb46a96e8abcf9dd1
RomanGit777 <rsavenko91@gmail.com>
2025-06-22 15:34:49 +0200

    working with main.js

---
+++ hw 3 additionally/#reLkOkTB29Q/scripts/main.js
@@ -0,0 +1,49 @@
+// створити масив книжок (назва, кількість сторінок, автори , жанри).
+//
+// – знайти найбільшу книжку.
+//
+// – знайти книжку/ки з найбільшою кількістю жанрів
+//
+// – знайти книжку/ки з найдовшою назвою
+//
+// – знайти книжку/ки, які писали 2 автори
+//
+// – знайти книжку/ки, які писав 1 автор
+const books = [
+    {
+        title: '1984',
+        pages: 328,
+        authors: ['Джордж Орвелл'],
+        genres: ['Політична фантастика', 'антиутопія', 'антиутопія']
+    },
+    {
+        title: 'ghghhggh',
+        pages: 4555,
+        authors: ['ghhghghgh'],
+        genres: ['dgdfdfgg', 'fgfggf']
+    },
+    {
+        title: 'jjjjjjjjj',
+        pages: 222,
+        authors: ['jjjjjjjjjj'],
+        genres: ['jhjhjhjh']
+    },
+    {
+        title: 'tttttttttttt',
+        pages: 111,
+        authors: ['ttttttttttttt'],
+        genres: ['kkkkkkkkkkkkk']
+    },
+    {
+        title: 'kkkkkkkkkkkk',
+        pages: 523,
+        authors: ['tttttttttttttttt', 'tttttttttttttttt'],
+        genres: ['gggggggggggggg']
+    },
+    {
+        title: 'ttttttttttt',
+        pages: 433,
+        authors: ['ggggggggggggggg'],
+        genres: ['hhhhhhhhhhhhhhhh']
+    }
+];
