c1bf24e7d4e6a9b11574db4fb17f80f216402cfc
RomanGit777 <rsavenko91@gmail.com>
2025-06-24 14:23:13 +0200

    working with main.js

--- hw 3 additionally/last one/scripts/main.js
+++ hw 3 additionally/last one/scripts/main.js
@@ -5,7 +5,7 @@ for (let i = 0; evenNumbers.length < 50; i++){
         evenNumbers.push(i);
     }
 }
-console.log('50 парних чисел:', evenNumbers);
+// console.log('50 парних чисел:', evenNumbers);
 
 // b. заповнити його 50 непарними числами за допомоги циклу.
 let oddNumbers = [];
@@ -14,7 +14,7 @@ for (let i = 0; oddNumbers.length < 50; i++){
         oddNumbers.push(i);
     }
 }
-console.log('50 непарних чисел:', oddNumbers);
+// console.log('50 непарних чисел:', oddNumbers);
 
 // c. Заповнити масив 20-ма рандомними числами.
 let randomNumbers = [];
@@ -29,4 +29,32 @@ for (let i = 0; i < 20; i++){
     let num = Math.floor(Math.random() * (732 - 8 + 1)) + 8;
     rangedRandom.push(num);
 }
-console.log('20 випадкових чисел від 8 до 732:', rangedRandom);
+// console.log('20 випадкових чисел від 8 до 732:', rangedRandom);
+
+// 2. Вивести за допомогою console.log кожен третій елемент
+// for (let i = 2; i < rangedRandom.length; i += 3){
+//     console.log('Третій елемент:', rangedRandom[i]);
+// }
+
+// 3. Вивести за допомогою console.log кожен третій елемент, тільки якщо цей елемент є парним.
+for (let i = 2; i < randomNumbers.length; i++){
+    if (i % 2 === 0){
+        console.log('Парний третій елемент:', randomNumbers[i]);
+    }
+}
+// 4. Вивести за допомогою console.log кожен третій елемент, тільки якщо цей елемент є парним, та записати їх в новий масив
+let evenThirds = [];
+for (i = 2; i < randomNumbers.length; i += 3){
+    if (randomNumbers[i] % 2 === 0){
+        evenThirds.push(randomNumbers[i]);
+        console.log('Додано до масиву:', randomNumbers[i]);
+    }
+}
+
+// 5. Вивести кожен елемент масиву, сусід справа якого є парним
+let arr = [1, 2, 3, 5, 7, 9, 56, 8, 67];
+for (let i = 0; i < arr.length - 1; i++){
+    if (arr[i + 1] % 2 === 0){
+        console.log('Елемент перед парним сусідом:', arr[i]);
+    }
+}
