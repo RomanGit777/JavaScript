2d66ee76f0ca82e136af86ed552afe24b00a783f
RomanGit777 <rsavenko91@gmail.com>
2025-06-27 19:20:26 +0200

    done with main.js

---
+++ hw 4/#ETGAxbEn8l/scripts/main.js
@@ -0,0 +1,7 @@
+// – створити функцію, яка обчислює та повертає площу кола з радіусом r
+function roundSquare(radius){
+    let result = Math.PI * radius * radius;
+    console.log(result);
+    return result;
+}
+roundSquare(3);
