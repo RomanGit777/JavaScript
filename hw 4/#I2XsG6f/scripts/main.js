e4682ed04b2732baeaee30744567d936f01273f3
RomanGit777 <rsavenko91@gmail.com>
2025-06-27 19:12:06 +0200

    done with main.js

---
+++ hw 4/#I2XsG6f/scripts/main.js
@@ -0,0 +1,7 @@
+// – створити функцію, яка обчислює та повертає площу прямокутника зі сторонами  а і б
+function square(aNum, bNum){
+    const result = aNum * bNum;
+    console.log(result);
+    return result;
+}
+square(5, 8)
