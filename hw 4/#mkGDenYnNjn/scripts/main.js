1747487acd36e57561f634056c87fde51693e04c
RomanGit777 <rsavenko91@gmail.com>
2025-07-16 14:30:26 +0200

    add changes to main.js

--- hw 4/#mkGDenYnNjn/scripts/main.js
+++ hw 4/#mkGDenYnNjn/scripts/main.js
@@ -1,14 +1,10 @@
 // Написати функцію обміну валюти exchange(sumUAH,currencyValues,exchangeCurrency)
 // Приклад exchange(10000,[{currency:’USD’,value:25},{currency:’EUR’,value:42}],’USD’) // => 400
-
-exchange(10000,[{currency:'USD',value:25},{currency:'EUR',value:42}],'USD') // => 400
-function exchange(sumUAH,currencyValues,exchangeCurrency) {
-    const currencyObj = currencyValues.find(item => item.currency === exchangeCurrency);
-    if (!currencyObj){
-        console.log('Валюта не знайдена');
-        return null;
+function exchange(sumUAH, currencyValues, exchangeCurrency) {
+    for (const item of currencyValues) {
+        if (item.currency === exchangeCurrency) {
+            return sumUAH / item.value;
+        }
     }
-    const result = sumUAH / currencyObj.value;
-    console.log(result);
-    return result;
 }
+console.log(exchange(10000, [{currency: 'USD', value: 25}, {currency: 'EUR', value: 42}], 'USD'));
