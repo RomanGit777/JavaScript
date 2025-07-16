07133827ad087dc21884e7dd9d27c98ef7e02e41
RomanGit777 <rsavenko91@gmail.com>
2025-07-16 21:41:59 +0200

    renamed directory and working with class work string function

---
+++ module 6/class work/scripts/main.js
@@ -0,0 +1,26 @@
+// string function :
+// let str = 'hello okten';
+// console.log(str);
+// console.log(str[6]);
+// let s = new String('qweewqew');
+// console.log(typeof s);
+// let arr1 = ['a', 'b', 'c'];
+// let arr2 = ['e', 'r', 'y'];
+// let b = arr1.concat(arr2);
+// console.log(b);
+// let s = str.concat('!!!') // функція яка дає нам асболютно новий об'єкт не міняючи при цьому перший
+// console.log(s);
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+// console.log(str.startsWith('hel'));
+// console.log(str.endsWith('en'));
+// console.log(str.substring(0,8));
+// console.log(str.indexOf('o'));
+// console.log(str.lastIndexOf('o'));
+// console.log(str.indexOf('o',5));
+// console.log(str.charAt(8));
+// // console.log(str.replace('o','!!!'));
+// console.log(str.replaceAll('o','!!'));
+// let split = str.split(' ');
+// console.log(split);
+
