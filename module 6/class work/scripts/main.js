5e023f859e32a1a0cc9fee484af9e2c6cbb376a8
RomanGit777 <rsavenko91@gmail.com>
2025-07-17 15:50:17 +0200

    working with class work

--- module 6/class work/scripts/main.js
+++ module 6/class work/scripts/main.js
@@ -25,3 +25,47 @@
 // console.log(split);
 
 // array function :
+let arr = [];
+console.log(Array.isArray(arr)); // перевірка чи є цей масив масивом
+arr.push(123);
+arr.push(44);
+arr.push(44);
+arr.push(44);
+console.log(arr);
+
+console.log(arr.pop()); // вирізає останній елемент в масиві та показує його, якщо логнути
+console.log(arr);
+
+console.log(arr.unshift('!!')); // додає елемент на початок масиву та показує його якщо логнути
+console.log(arr);
+
+console.log(arr.shift()); //видаляє перший елемент з масиву та показує його якщо логнути
+console.log(arr);
+
+let join = arr.join(); // з'єдную кожен елемент масиву по дефолту комою
+console.log(join);
+
+let nums = [11,22,33]; //
+let concat = arr.concat(nums); // викликаємо конкат та додаємо до масива новий масив
+console.log(concat); // повертається з'єднаний новий масив
+console.log(arr); // старий масив залишається незміненим
+
+console.log(nums.reverse()); // перевертає масив
+
+console.log(concat);
+console.log(concat.slice(0,4)); // вирізає елементи масиву
+console.log(concat); // оригінальній масив не змінюється
+
+// let splice = concat.splice(0,2, '!!!','!!!','!!!',); //дозволяє видаляти елементи з масиву, 1 та другий аргумент =
+// // звідки починати та до куди йти, 3 аргумент = що добавити замість видалених елементів.
+// console.log(splice);
+// console.log(concat);
+
+console.log(concat);
+console.log(concat.indexOf(11)); // дозволяє побачити під яким індексом стоїть названий елемент у аргументі
+console.log(concat);
+concat.splice(3,1) // дозволяє видалити елемент, 1 число = індекс з якого починати, 2 число = скільки елементів видалити
+console.log(concat);
+
+console.log(concat.includes(22)); // видає або тру або фолс відходячи від того чи є такий елемент у масиві
+console.log(concat.includes(11)); //
