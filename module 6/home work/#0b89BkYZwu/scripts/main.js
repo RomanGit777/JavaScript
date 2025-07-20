175f8d288bc335226fd71deb7a16b4d6aa9314e5
RomanGit777 <rsavenko91@gmail.com>
2025-07-20 17:00:46 +0200

    working with hw task 4

---
+++ module 6/home work/#0b89BkYZwu/scripts/main.js
@@ -0,0 +1,8 @@
+// #0b89BkYZwu
+// – Є “брудна” стрінга let str = ' dirty string   ' . Почистити її від зайвих пробілів.
+let str = ' dirty string   '
+console.log(str);
+console.log(str.length);
+const s = str.trim();
+console.log(s);
+console.log(s.length);
