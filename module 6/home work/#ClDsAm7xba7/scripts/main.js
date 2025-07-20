52514d2e531801e8512a009534a364a723e8301d
RomanGit777 <rsavenko91@gmail.com>
2025-07-20 16:52:10 +0200

    working with hw task 3

---
+++ module 6/home work/#ClDsAm7xba7/scripts/main.js
@@ -0,0 +1,13 @@
+// #ClDsAm7xba7
+// – Перевести до нижнього регістру наступні стрінгові значення
+//       'HELLO WORLD', 'LOREM IPSUM', 'JAVASCRIPT IS COOL'
+let a = 'HELLO WORLD';
+const s1 = a.toLowerCase();
+console.log(s1);
+let b = 'LOREM IPSUM';
+const s2 = b.toLowerCase();
+console.log(s2);
+let c = 'JAVASCRIPT IS COOL';
+const s3 = c.toLowerCase();
+c = s3;
+console.log(c);
