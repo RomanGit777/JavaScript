9140e528ada3c275ec0e00e073afa7731aea02da
RomanGit777 <rsavenko91@gmail.com>
2025-07-20 16:43:49 +0200

    working with hw task 2

---
+++ module 6/home work/#8lld9HMxXWB/scripts/main.js
@@ -0,0 +1,12 @@
+// #8lld9HMxXWB
+// – Перевести до великого регістру наступні стрінгові значення
+//'hello world', 'lorem ipsum', 'javascript is cool'
+let s = 'hello world';
+const s1 = s.toUpperCase();
+console.log(s1);
+let a = 'lorem ipsum';
+const s2 = a.toUpperCase();
+console.log(s2);
+let b = 'javascript is cool';
+const s3 = b.toUpperCase();
+console.log(s3);
