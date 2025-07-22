c4ea5a71244a6e8c7bede3b848d02f9ccebb14f0
RomanGit777 <rsavenko91@gmail.com>
2025-07-22 17:19:39 +0200

    working with hw task 8

--- module 6/home work/#yo06d74c1C/scripts/main.js
+++ module 6/home work/#yo06d74c1C/scripts/main.js
@@ -12,17 +12,19 @@
 //  — відфільтрувати, залишивши тільки курси з тривалістю понад 5 місяців
 //  — за допомоги map перетворити кожен елемент на наступний тип {id,title,monthDuration}
 
-//  — відсортувати його за спаданням за monthDuration
-// function sortedArray(array,direction) {
-//     if (direction === 'ascending'){array.monthDuration.sort((a, b) => a - b)
-//     }}
-//
-// let coursesAndDurationArray = [
-//     {title: 'JavaScript Complex', monthDuration: 5},
-//     {title: 'Java Complex', monthDuration: 6},
-//     {title: 'Python Complex', monthDuration: 6},
-//     {title: 'QA Complex', monthDuration: 4},
-//     {title: 'FullStack', monthDuration: 7},
-//     {title: 'Frontend', monthDuration: 4}
-// ];
-// console.log(sortedArray(coursesAndDurationArray));
+const coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+
+// const map1 = coursesAndDurationArray
+//     .sort((a, b) => b.monthDuration - a.monthDuration)
+//     .filter(value => value.monthDuration > 5)
+//     .map((value, index) => ({...value, id: index + 1}));
+// console.log(map1);
+
+
