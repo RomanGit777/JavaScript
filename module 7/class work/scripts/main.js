205fe81028f692fa790b701a06c3f40f468712ff
RomanGit777 <rsavenko91@gmail.com>
2025-07-27 13:48:18 +0200

    add directory 7 and work with class work

---
+++ module 7/class work/scripts/main.js
@@ -0,0 +1,27 @@
+// function User(name,age,status){
+//     this.name = name;
+//     this.age = age;
+//     this.status = status;
+// }
+// let user1 = new User('vasya',15,true);
+// console.log(user1);
+// let user2 = new User('kolya',18,false);
+// console.log(user2);
+
+// function User(name, age, status, wife) {
+//     this.name = name;
+//     this.age = age;
+//     this.status = status;
+//     this.wife = wife;
+// }
+// let user1 = new User('vasya', 22, true, {name: 'larisa', age: 20, status: true});
+// console.log(user1);
+
+// function User(name, age, status, wifeName, wifeAge, wifeStatus ) {
+//     this.name = name;
+//     this.age = age;
+//     this.status = status;
+//     this.wife = {name: wifeName, age: wifeAge, status: wifeStatus};
+// }
+// let user1 = new User('vasya', 22, true, 'larisa',20,true);
+// console.log(user1);
