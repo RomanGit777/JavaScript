ed38be4d65307d7f68ed82865367444edee93c50
RomanGit777 <rsavenko91@gmail.com>
2025-07-31 12:12:09 +0200

    work with class work

--- module 7/class work/scripts/main.js
+++ module 7/class work/scripts/main.js
@@ -30,10 +30,10 @@
 // user.greeting = () =>  `hello ${this.name}`;
 // console.log(user.greeting());
 
-function User(name,age,){
-    this.name = name;
-    this.age = age;
-}
+// function User(name,age,){
+//     this.name = name;
+//     this.age = age;
+// }
 //
 // let user = new User('vasya', 31);
 // user.greeting = function(){
@@ -42,9 +42,31 @@ function User(name,age,){
 // console.log(user);
 // user.greeting();
 
-console.log(User);
-User.prototype.greeting = function () {
-    return `Hello my name is ${this.name}!`;
-};
-let user = new User('vasya',31);
-console.log(user.greeting());
+// console.log(User);
+// User.prototype.greeting = function () {
+//     return `Hello my name is ${this.name}!`;
+// };
+// let user = new User('vasya',31);
+// console.log(user.greeting());
+
+// function User (name,age,status,wifeName,wifeAge,wifeStatus){
+//     this.name = name;
+//     this.age = age;
+//     this.status = status;
+//     this.wife = {name : wifeName, age : wifeAge, status : status};
+//     this.greeting = function(){
+//         console.log(`hi ${this.name}!`);
+//         console.log(`hi ${this.wife.name}!`);
+//         console.log(`Your status is : ${this.status}!`);
+//         console.log(`Your status is : ${this.wife.status}!`);
+//     }
+// }
+// let user1 = new User('John', 22, true, 'Anna', 22, true);
+// console.log(user1);
+// user1.greeting();
+// let user2 = new User('Ronaldo', 25, false, 'rob', 21, false);
+// console.log(user2);
+// user2.greeting();
+// let user3 = new User('John', 19, true, 'olya', 20, true);
+// console.log(user3);
+// user3.greeting();
