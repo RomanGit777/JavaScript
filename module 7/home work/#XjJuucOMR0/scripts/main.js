65689ac986ba3c079c8d0b79ac8afc17d05637ad
RomanGit777 <rsavenko91@gmail.com>
2025-08-01 13:48:24 +0200

    add changes to task 1

--- module 7/home work/#XjJuucOMR0/scripts/main.js
+++ module 7/home work/#XjJuucOMR0/scripts/main.js
@@ -10,15 +10,15 @@ this.email = email;
 this.phone = phone;
 }
 let users = [
-new User(1, 'vasya','olekseev','eproerop@gmail.com',+12313),
-new User(2, 'fdfd','fdfdf','fdfd@gmail.com',+2323),
-new User(3, 'rttr','dffd','hjjjh@gmail.com',+87887),
-new User(4, 'hghg','gfgf','dffdfd@gmail.com',+56676776),
-new User(5, 'bvbvvb','kllklk','hjhjh@gmail.com',+8787),
-new User(6, 'hghgh','bvvbv','fdfdfd@gmail.com',+77667),
-new User(7, 'fggfg','hjjhhj','hghg@gmail.com',+7676),
-new User(8, 'dffdfd','dgdfd','dddsds@gmail.com',+4344366),
-new User(9, 'trtr','fgfgf','jkkjkj@gmail.com',+4554455),
-new User(10, 'qwe','ert','fgf@gmail.com',+21323213),
+new User(1, 'vasya','olekseev','eproerop@gmail.com','+12313'),
+new User(2, 'fdfd','fdfdf','fdfd@gmail.com','+2323'),
+new User(3, 'rttr','dffd','hjjjh@gmail.com','+87887'),
+new User(4, 'hghg','gfgf','dffdfd@gmail.com','+56676776'),
+new User(5, 'bvbvvb','kllklk','hjhjh@gmail.com','+8787'),
+new User(6, 'hghgh','bvvbv','fdfdfd@gmail.com','+77667'),
+new User(7, 'fggfg','hjjhhj','hghg@gmail.com','+7676'),
+new User(8, 'dffdfd','dgdfd','dddsds@gmail.com','+4344366'),
+new User(9, 'trtr','fgfgf','jkkjkj@gmail.com','+4554455'),
+new User(10, 'qwe','ert','fgf@gmail.com','+21323213'),
 ]
 console.log(users);
