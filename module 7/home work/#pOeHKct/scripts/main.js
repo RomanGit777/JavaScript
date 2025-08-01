d1a25d1329261ddc6ac2e66d25862a6a48aa069d
RomanGit777 <rsavenko91@gmail.com>
2025-08-01 17:54:21 +0200

    done with task 3

---
+++ module 7/home work/#pOeHKct/scripts/main.js
@@ -0,0 +1,25 @@
+// #pOeHKct
+// – Взяти масив з  User[] з попереднього завдання, та відсортувати його по id. по зростанню (sort)
+function User(id, name, surname, email, phone) {
+    this.id = id;
+    this.name = name;
+    this.surname = surname;
+    this.email = email;
+    this.phone = phone;
+}
+
+let users = [
+    new User(2, 'fdfd', 'fdfdf', 'fdfd@gmail.com', '+2323'),
+    new User(6, 'hghgh', 'bvvbv', 'fdfdfd@gmail.com', '+77667'),
+    new User(4, 'hghg', 'gfgf', 'dffdfd@gmail.com', '+56676776'),
+    new User(9, 'trtr', 'fgfgf', 'jkkjkj@gmail.com', '+4554455'),
+    new User(5, 'bvbvvb', 'kllklk', 'hjhjh@gmail.com', '+8787'),
+    new User(10, 'qwe', 'ert', 'fgf@gmail.com', '+21323213'),
+    new User(1, 'vasya', 'olekseev', 'eproerop@gmail.com', '+12313'),
+    new User(7, 'fggfg', 'hjjhhj', 'hghg@gmail.com', '+7676'),
+    new User(8, 'dffdfd', 'dgdfd', 'dddsds@gmail.com', '+4344366'),
+    new User(3, 'rttr', 'dffd', 'hjjjh@gmail.com', '+87887'),
+];
+
+
+console.log(users.sort((u1, u2) => u1.id - u2.id));
