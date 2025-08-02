1ab948dd96bcf656d1e00f58fc404dd40b80f9f9
RomanGit777 <rsavenko91@gmail.com>
2025-08-02 14:18:21 +0200

    done with task 6

--- module 7/home work/#vV9a6584I5/scripts/main.js
+++ module 7/home work/#vV9a6584I5/scripts/main.js
@@ -7,40 +7,41 @@
 // — increaseMaxSpeed(newSpeed) – яка підвищує значення максимальної швидкості на значення newSpeed
 // — changeYear(newValue) – змінює рік випуску на значення newValue
 // — addDriver (driver) – приймає об’єкт який “водій” з довільним набором полів, і додає його в поточний об’єкт car
-function Car(model, producer, year, maxSpeed, engineVolume) {
+function Car(model,producer,year,maxSpeed,engineVolume){
     this.model = model;
     this.producer = producer;
     this.year = year;
     this.maxSpeed = maxSpeed;
     this.engineVolume = engineVolume;
 
-    this.drive = function () {
-        console.log(`їдемо зі швидкістю ${this.maxSpeed} на годину`); // just log our speed
+    this.drive = function(){
+        console.log(`їдемо зі швидкістю ${maxSpeed} на годину`);
     };
 
-    this.info = function () {
-        for (const key in this) console.log(key, this[key]); // using for in to loop through all info and log
-        // field name, value and all info inside our car constructor
+    this.info = function(){
+        for (const key in this) {
+            console.log(key, this[key])
+        }
     };
 
-    this.increaseMaxSpeed = function (speedToAdd) {
-        if (this.maxSpeed > 0) this.maxSpeed = this.maxSpeed + speedToAdd;
-    }; // we can update our exist speed to higher value
+    this.increaseMaxSpeed = function(speedToAdd){
+        if(this.maxSpeed > 0) this.maxSpeed = this.maxSpeed + speedToAdd;
+    };
 
-    this.changeYear = function (year) {
-        if (year > 1815) this.year = year;
-    }; // we can update our year using this function
+    this.changeYear = function(year){
+        if(year > 1815) this.year = year;
+    };
 
-    this.addDriver = function (driver) {
-        if (driver) this.driver = driver;
-    }; // we can add driver object to our info about a car
+    this.addDriver = function(driver){
+        if(driver) this.driver = driver;
+    };
 }
 
-const car = new Car('qwe', 'qwef', 2012, 120, 4);
+const car = new Car('qwe', 'qwer', 1987, 120, 100);
 console.log(car);
 car.drive();
 car.info();
-car.increaseMaxSpeed(20);
-car.changeYear(2000);
-car.addDriver({name: 'qwef', age: 22});
+car.increaseMaxSpeed(80);
+car.changeYear(2005);
+car.addDriver({name : 'qwe', age: 22});
 console.log(car);
