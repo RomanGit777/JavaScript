d71df0deee41ea464c92b7661982dc1901e4e52e
RomanGit777 <rsavenko91@gmail.com>
2025-08-06 09:48:32 +0200

    working with class work

--- module 10/class work/scripts/main.js
+++ module 10/class work/scripts/main.js
@@ -46,14 +46,54 @@
 //     console.log(user);
 // }
 
-window.onload = function () {
-    console.log("Load");
-}
+// window.onload = function () {
+//     console.log("Load");
+// }
+//
+// document.onreadystatechange = function () {
+//     if (document.readyState === "interactive") {
+//         document.body.innerText = 'Loading start...'
+//     }else if (document.readyState === "complete") {
+//         document.body.innerText = 'Loading complete'
+//     }
+// }
 
-document.onreadystatechange = function () {
-    if (document.readyState === "interactive") {
-        document.body.innerText = 'Loading start...'
-    }else if (document.readyState === "complete") {
-        document.body.innerText = 'Loading complete'
-    }
-}
+// localStorage.setItem('Hello', 'World!');
+// const key = localStorage.getItem('Hello');
+// console.log(key);
+// localStorage.clear();
+
+
+// localStorage.setItem('user', JSON.stringify({id: 1, name: 'kokos'}));
+
+// let userJSON = localStorage.getItem('user');
+// console.log(userJSON);
+// let user = JSON.parse(userJSON);
+// console.log(user);
+// user.age = 31;
+// console.log(user);
+// localStorage.setItem('user', JSON.stringify(user));
+
+// let users = [
+//     {name: 'vasya', age: 31, status: false},
+//     {name: 'petya', age: 30, status: true},
+//     {name: 'kolya', age: 29, status: true},
+//     {name: 'olya', age: 28, status: false},
+//     {name: 'max', age: 30, status: true},
+//     {name: 'anya', age: 31, status: false},
+//     {name: 'oleg', age: 28, status: false},
+//     {name: 'andrey', age: 29, status: true},
+//     {name: 'masha', age: 30, status: true},
+//     {name: 'olya', age: 31, status: false},
+//     {name: 'max', age: 31, status: true}
+// ];
+//
+// localStorage.setItem('users', JSON.stringify(users));
+
+// let usersJSON = localStorage.getItem('users');
+// console.log(usersJSON);
+// let users = JSON.parse(usersJSON);
+// console.log(users);
+// users.push({});
+// console.log(users);
+// localStorage.setItem('users', JSON.stringify(users));
