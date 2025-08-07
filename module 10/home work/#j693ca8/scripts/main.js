1f5d492d1826ea474f7dcb89e000ba0bda1eeeab
RomanGit777 <rsavenko91@gmail.com>
2025-08-07 11:18:10 +0200

    working with task 2

--- module 10/home work/#j693ca8/scripts/main.js
+++ module 10/home work/#j693ca8/scripts/main.js
@@ -1,9 +1,18 @@
 // #j693ca8
-// – створити інпут, який приймає вік людини, та кнопку, яка підтверджує дію. При натисканні на кнопку зчитати інформацію з інпуту та перевірити вік, чи менше він за 18, та повідомити про це користувача
+// – створити інпут, який приймає вік людини, та кнопку, яка підтверджує дію. При натисканні на кнопку
+// зчитати інформацію з інпуту та перевірити вік, чи менше він за 18, та повідомити про це користувача
 
 const form = document.forms.f1;
-form.onclick = function () {
-    const inpt = document.getElementById('input');
-    let result = inpt.value >= 18 || document.body.appendChild('you dont have access, your age is under 18');
-    document.body.appendChild('result');
+const message = document.getElementById('message');
+
+form.onsubmit = function (e) {
+    e.preventDefault();
+   const age = form.age.value;
+
+   if (age < 18) {
+       message.innerText = 'You are under 18!'
+   }
+   if (age >= 18) {
+       message.innerText = 'Welcome!'
+   }
 }
